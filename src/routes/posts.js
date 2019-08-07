33e3ff66383c301faec35f710d9cb1be6a79d156
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-08-06 22:48:35 -0400

    GET /api/posts

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,3 +1,21 @@
+const db = require('../database/db-config');
 const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const posts = await db('posts');
+
+    res.status(200).json({ 
+      posts, 
+      success: true 
+    });
+  } catch (error) {
+    res.status(500).json({ 
+      error, 
+      message: "Posts could not be retrieved.", 
+      success: false 
+    });
+  }
+});
 
 module.exports = router;
